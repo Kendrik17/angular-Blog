62d8fcbc5e89aa75363d558b7aa3ed4b235c9e6e
Kendrik G. Duarte <kendrikatx1@gmail.com>
2024-01-14 15:50:33 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './c/card/card.component';
+import { BigCardComponent } from './c/big-card/big-card.component';
+import { MenuBarComponent } from './c/menu-bar/menu-bar.component';
+import { TitleComponent } from './c/title/title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    BigCardComponent,
+    MenuBarComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule,
